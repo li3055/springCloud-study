1204f12728e485d0fcd94a8dacf57fefdbb4944c
867816042@qq.com <qazwsx12>
2017-12-18 22:05:13 +0800

    aaa

--- docker-eureka/src/main/java/com/rrjc/DemoApplication.java
+++ docker-eureka/src/main/java/com/rrjc/DemoApplication.java
@@ -7,7 +7,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class DemoApplication {
-
+	/**
+	 * dd
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
